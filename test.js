0bcc664cb207fd4e57aef7acd9dbb30090ccf711
Ravi Patel <ravipate@buffalo.edu>
2019-07-11 01:22:38 +0000

    cleaned-up tests

--- test.js
+++ test.js
@@ -1,75 +1,81 @@
 /* eslint-disable brace-style, camelcase, semi */
 /* eslint-env mocha */
 
-require('dotenv').config();
-
 var chai = require('chai');
 var assert = chai.assert;
 
 const config = {
   domain: 'funnode.com',
   key: '131133',
-  live: process.env.NODE_ENV === 'production'
+  live: false
 };
 
-describe('Mailer', function () {
-  var mailer = new (require('./index.js'))(config.domain, config.key, config.live);
-  var msg = { text: `test message` };
-  var returned_message = mailer.queue(msg);
+describe('Mailer', () => {
+  let mailer = new (require('./index.js'))(config.domain, config.key, config.live);
+  let msg = { text: `test message` };
+  let returned_message = mailer.queue(msg);
   mailer.time_interval = 1000;
 
-  describe('Queues Messages', function () {
-    it('Should Return the Same Reference Object', function (done) {
-      assert.equal(returned_message, msg, 'Message was not returned correctly');
+  describe('Queue Messages', () => {
+    it('should return the same referenced object', (done) => {
+      assert.equal(returned_message, msg);
       done();
     });
-    it('Should Queue in the Array Correctly', function (done) {
-      assert.equal(mailer.queued.length, 1, 'message.queued is not the correct length')
+
+    it('should queue in the array', (done) => {
+      assert.equal(mailer.queued.length, 1)
       done();
     });
-    it('Should Be Present in the Queue', function (done) {
-      assert.equal(mailer.queued[0], msg, 'Message was not in the queue');
+
+    it('should be present in the queue', (done) => {
+      assert.equal(mailer.queued[0], msg);
       done();
     });
-    it('Should Have a Timer to Send Messages', function (done) {
-      assert.notEqual(mailer.timeout, false, 'Timeout was not set');
+
+    it('should have a timer to send messages', (done) => {
+      assert.notEqual(mailer.timeout, false);
       done();
     });
-    it('Should Return False with Empty Quotes', function (done) {
-      assert.equal(mailer.queue({ text: `` }), false, 'Mailer should return false with empty quotes');
+
+    it('should return false for empty message', (done) => {
+      assert.equal(mailer.queue({ text: `` }), false);
       done();
     });
   });
 
-  describe('Prepares Messages', function () {
-    it('Prepares Message Subject with Date', function (done) {
-      var today = new Date();
+  describe('Prepare Messages', () => {
+    it('should set subject and date', (done) => {
+      let today = new Date();
       today = `${today.getMonth() + 1}-${today.getDate()}`;
-      assert.notEqual(returned_message.subject.indexOf(today), -1, 'The current date should be in the message subject');
+      assert.notEqual(returned_message.subject.indexOf(today), -1);
       done();
     });
-    it('Prepares Message Sender to no-reply@funnode.com', function (done) {
-      assert.equal(returned_message.from, 'no-reply@funnode.com', 'Message \'from\' is not \'no-reply@funnode.com\'');
+
+    it('should set sender to no-reply@funnode.com', (done) => {
+      assert.equal(returned_message.from, 'no-reply@funnode.com');
       done();
     });
-    it('Sets Message Prepared Flag', function (done) {
-      assert.equal(returned_message.prepared, true, 'message.prepared flag should be set to true after being prepared');
+
+    it('should set prepared flag', (done) => {
+      assert.equal(returned_message.prepared, true);
       done();
     });
   });
 
-  describe('Sends Queued Messages', function () {
-    it('Queue should accept 2 messages', function (done) {
-      var msg2 = { 'text': 'test message 2' };
+  describe('Send Queued Messages', () => {
+    it('should accept two messages', (done) => {
+      let msg2 = { 'text': 'test message 2' };
       mailer.queue(msg2);
-      assert.equal(mailer.queued.length, 2, 'Mailer did not queue the second message');
+      assert.equal(mailer.queued.length, 2);
       done();
     });
-    it('Queue should send the 2 messages', function (done) {
+
+    it('should send two messages', (done) => {
       this.timeout(mailer.time_interval + 500);
-      console.log('This next test may take ' + (mailer.time_interval + 100) / 1000 + 'seconds')
-      setTimeout(function () {
-        assert.equal(mailer.queued.length, 0, 'Mailer kept the messages in its queue, message likely not sent');
+      console.log('This next test may take ' + (mailer.time_interval + 100) / 1000 + 'seconds');
+
+      setTimeout(() => {
+        assert.equal(mailer.queued.length, 0);
         done();
       }, mailer.time_interval + 50);
     });
